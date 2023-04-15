81664ae4c17c501e08c61a694bc58666cf5fe616
AntonAnatoliyovich <anton.shadura@gmail.com>
2023-04-15 21:03:05 +0300

    init

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import Header from "./Header/Header";
+import Home from "pages/Home/Home";
+import Movies from "pages/Movies/Movies";
+import MovieDetails from "./MovieDetails/MovieDetails";
+import Cast from "./Cast/Cast";
+import Reviews from "./Reviews/Reviews";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="App">
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies/>} />
+        <Route path="/movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+      </Routes>
     </div>
   );
 };
---
+++ src/components/Header/Header.styled.js
@@ -0,0 +1,34 @@
+import styled from '@emotion/styled';
+import { NavLink } from "react-router-dom";
+
+export const HeaderElement = styled.header`
+    min-width: 100vw;
+    height: 70px;
+    box-shadow: 0 0 15px #888888;
+    margin-bottom: 20px;
+`;
+
+export const HeaderInner = styled.div`
+    height: 100%;
+`;
+
+export const HeaderNavigation = styled.nav`
+    height: 100%;
+`;
+
+export const HeaderLinksList = styled.ul`
+    display: flex;
+    align-items: center;
+    height: 100%;
+    gap: 20px;
+`;
+
+export const HeaderLink = styled.li``;
+
+export const StyledNavLink = styled(NavLink)`
+    color: #000;
+    font-weight: 600;
+    &.active {
+        color: tomato;
+    }
+`;
